a25ae851a4bd8eaf399bc61335097bfc750bef79
darkred974 <lallemandguillaume55@gmail.com>
2026-02-08 20:18:12 +0400

    Updates

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-3IZMSAWD.mjs";import"./chunk-WZ6MWS4F.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-HYHPXFQT.mjs";import"./chunk-WZ6MWS4F.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
